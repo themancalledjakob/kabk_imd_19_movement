e6c5c8a34e0855416f2bc1d5d1c3f7595d591cd1
themancalledjakob <bornbreaker@gmx.de>
2019-03-10 12:32:53 +0100

    added phonesensor

---
+++ unity/phoneSensor/Assets/SensorSaver.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+public class SensorSaver : MonoBehaviour {
+
+    // this is the data we want to save
+    [Serializable]
+    public class SensorData
+    {
+        public string timeStamp;
+        public Vector3 rotationRate;
+        public Vector3 userAcceleration;
+        public Vector3 deviceAcceleration;
+        public Vector3 gravity;
+        public float magneticHeading;
+        public float trueHeading;
+        public float latitude;
+        public float longitude;
+        public float altitude;
+    }
+
+    public Camera cam;
+    public Transform example;
+
+    System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+    long lastSave;
+    public long saveIntervalMillis = 1000;
+
+    string savePath;
+
+    List<SensorData> sensorDataList;
+
+    // Use this for initialization
+    void Start () {
+        sensorDataList = new List<SensorData>();
+        cam.backgroundColor = Color.white;
+
+        stopwatch.Start ();
+        lastSave = stopwatch.ElapsedMilliseconds;
+
+        Input.compass.enabled = true;
+        Input.gyro.enabled = true;
+        Input.location.Start();
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        // take default data path for saving your data
+        savePath = Application.persistentDataPath;
+
+        // you could also try to create a special directory,
+        // but this might not work on your phone
+        // System.IO.Directory.CreateDirectory(savePath);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        cam.backgroundColor = Color.black;
+
+        // transform the example cube, just to show that it works
+        example.localScale = Input.gyro.userAcceleration + new Vector3(1, 1, 1);
+
+        long now = stopwatch.ElapsedMilliseconds;
+        if (now > lastSave + saveIntervalMillis) {    
+            lastSave = now;
+
+            SensorData sensorData = new SensorData();
+
+
+            sensorData.timeStamp = DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss:fff");
+            sensorData.rotationRate = Input.gyro.rotationRate;
+            sensorData.userAcceleration = Input.gyro.userAcceleration;
+            sensorData.gravity = Input.gyro.gravity;
+            sensorData.deviceAcceleration = Input.acceleration;
+            sensorData.magneticHeading = Input.compass.magneticHeading;
+            sensorData.trueHeading = Input.compass.trueHeading;
+            sensorData.longitude = Input.location.lastData.longitude;
+            sensorData.latitude = Input.location.lastData.latitude;
+            sensorData.altitude = Input.location.lastData.altitude;
+
+
+            sensorDataList.Add(sensorData);
+            SaveData(sensorData);
+
+            Debug.Log(sensorData.timeStamp);
+
+            cam.backgroundColor = Color.white;
+        }
+    }
+
+
+    void OnGUI()
+    {
+        // Make a background box
+        GUI.Box(new Rect(10, 10, 440, 60), "Where is the data?");
+
+        GUI.Label(new Rect(30, 40, 400, 20), savePath);
+
+    }
+
+    void SaveData(SensorData sensorData)
+    {
+        string fileName = "sensorData_" + DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss:fff") + ".json";
+        string fullDestination = Path.Combine(savePath, fileName);
+
+        File.WriteAllText(fullDestination, JsonUtility.ToJson(sensorData));
+    }
+}
+
---
+++ unity/phoneSensor/Assets/SensorSender.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Collections.Generic;
+using UnityOSC;
+
+public class SensorSender : MonoBehaviour {
+
+	public Camera cam;
+
+	public Transform example;
+
+	System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+	long lastSend;
+	public long sendInterval = 30;
+
+	OSCClient osc;
+
+	IPAddress ipaddress;
+
+    public string receiverIP = "192.168.1.255";
+    public int receiverPort = 8050;
+
+    // Use this for initialization
+    void Start () {
+        osc = new OSCClient (IPAddress.Parse (receiverIP), receiverPort);
+		cam.backgroundColor = Color.black;
+
+		stopwatch.Start ();
+		lastSend = stopwatch.ElapsedMilliseconds;
+
+		Input.compass.enabled = true;
+		Input.gyro.enabled = true;
+		Input.location.Start();
+
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+        // transform the example cube, just to show that it works
+        example.localScale = Input.gyro.userAcceleration + new Vector3(1, 1, 1);
+
+        long now = stopwatch.ElapsedMilliseconds;
+		if (now > lastSend + sendInterval) {
+			
+			lastSend = now;
+
+			example.localRotation = Input.gyro.attitude;
+
+			Send ("/gyro/attitude", Input.gyro.attitude);
+			Send ("/gyro/rotationRate", Input.gyro.rotationRate);
+			Send ("/gyro/acceleration", Input.gyro.userAcceleration);
+			Send ("/gyro/gravity", Input.gyro.gravity);
+			Send ("/compass/mag", Input.compass.magneticHeading);
+			Send ("/compass/true", Input.compass.trueHeading);
+			Send ("/compass/raw", Input.compass.rawVector);
+			Send ("/compass/accuracy", Input.compass.headingAccuracy);
+			Send ("/acceleration", Input.acceleration);
+			Send ("/touchCount", Input.touchCount);
+		}
+	}
+
+	void Send(string address, Vector3 v) {
+		Send (address, new List<object> { v.x, v.y, v.z });
+	}
+
+	void Send(string address, Vector4 v) {
+		Send (address, new List<object> { v.x, v.y, v.z, v.w });
+	}
+
+	void Send(string address, Quaternion q) {
+		Send (address, new List<object> { q.x, q.y, q.z, q.w });
+	}
+
+	void Send(string address, float f) {
+		Send (address, new List<object> { f });
+	}
+
+	void Send(string address, int i) {
+		Send (address, new List<object> { i });
+	}
+
+	void Send(string address, List<object> args) {
+		{
+			OSCMessage msg = new OSCMessage (address);
+			for (var i = 0; i < args.Count; i++) {
+				object arg = args [i];
+				msg.Append (arg);
+			}
+			osc.Send (msg);
+		}
+	}
+
+	Vector3 smoothed(Vector3 v1, Vector3 v2, float factor) {
+		return v1 * factor + v2 * (1.0F - factor);
+	}
+}
+
